8b3193cf47795102c8b5c3264a220612394db03a
muhammdasif-wd <muhammadasif.wd@gmail.com>
2024-08-10 22:00:23 +0600

    [FIX] - server crashed problem

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,53 @@
-import { Server } from 'http'
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
-import { errorlogger, logger } from './shared/logger'
+import {Server} from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+import {errorlogger, logger} from "./shared/logger";
 
-process.on('uncaughtException', error => {
-  errorlogger.error(error)
-  process.exit(1)
-})
+process.on("uncaughtException", (error) => {
+  errorlogger.error("Uncaught Exception:", error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 
-//database connection
+// Database connection
 async function bootstrap() {
   try {
-    await mongoose.connect(`${config.database_url}`)
-    logger.info(`🛢 Database connection successful`)
-    app.listen(config.port, () => {
-      logger.info(
-        `Application listening on port http://localhost:${config.port}`,
-      )
-    })
+    await mongoose.connect(`${config.database_url}`);
+    logger.info("🛢 Database connection successful");
+    server = app.listen(config.port, () => {
+      logger.info(`Application listening on port http://localhost:${config.port}`);
+    });
+
+    server.on("error", (err) => {
+      errorlogger.error("Server error:", err);
+      process.exit(1);
+    });
   } catch (err) {
-    errorlogger.error(`Failed to connect database`, err)
+    errorlogger.error("Failed to connect to the database:", err);
+    process.exit(1);
   }
 
-  process.on('unhandledRejection', error => {
+  process.on("unhandledRejection", (error) => {
+    errorlogger.error("Unhandled Rejection:", error);
     if (server) {
       server.close(() => {
-        errorlogger.error(error)
-        process.exit(1)
-      })
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
 
-bootstrap()
+bootstrap();
 
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received')
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM is received");
   if (server) {
-    server.close()
+    server.close(() => {
+      logger.info("Server closed");
+    });
   }
-})
+});
