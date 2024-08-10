22a55eb954c9aed29c2b79f861b93dafc7af11f3
muhammdasif-wd <muhammadasif.wd@gmail.com>
2024-08-10 19:30:42 +0600

    [FEAT] - experience service route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import {SocialRoutes} from "../modules/home/social/social.route";
 import {SubmenuRoutes} from "../modules/home/submenu/submenu.route";
 import {fileUploadRoutes} from "../modules/file/file.route";
 import {SkillRoutes} from "../modules/home/skill/skill.route";
+import {ExperienceRoutes} from "../modules/home/experience/experience.route";
 const router = express.Router();
 
 const moduleRoutes = [
@@ -11,6 +12,7 @@ const moduleRoutes = [
   {path: "/submenu", route: SubmenuRoutes},
   {path: "/social", route: SocialRoutes},
   {path: "/skill", route: SkillRoutes},
+  {path: "/experience", route: ExperienceRoutes},
   {path: "/file", route: fileUploadRoutes},
 ];
 
