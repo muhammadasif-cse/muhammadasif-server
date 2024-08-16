2de5fc7af05c8b44895fa1ab732a4a5849849482
muhammdasif-wd <muhammadasif.wd@gmail.com>
2024-08-16 12:05:32 +0600

    [BULD] - for publish

--- dist/app/routes/index.js
+++ dist/app/routes/index.js
@@ -4,9 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const navigation_route_1 = require("../modules/home/navbar/navigation.route");
 const social_route_1 = require("../modules/home/social/social.route");
-const submenu_route_1 = require("../modules/home/submenu/submenu.route");
 const file_route_1 = require("../modules/file/file.route");
 const skill_route_1 = require("../modules/home/skill/skill.route");
 const experience_route_1 = require("../modules/home/experience/experience.route");
@@ -14,8 +12,6 @@ const testimonial_route_1 = require("../modules/home/testimonial/testimonial.rou
 const contact_route_1 = require("../modules/home/contact/contact.route");
 const router = express_1.default.Router();
 const moduleRoutes = [
-    { path: "/navigation", route: navigation_route_1.NavigationRoutes },
-    { path: "/submenu", route: submenu_route_1.SubmenuRoutes },
     { path: "/social", route: social_route_1.SocialRoutes },
     { path: "/skill", route: skill_route_1.SkillRoutes },
     { path: "/experience", route: experience_route_1.ExperienceRoutes },
