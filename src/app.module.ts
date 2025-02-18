34ef4b54638f1eacb412c3285b5c2c07062dbc18
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-02-18 22:59:49 +0600

    feat: enhance app module with controllers and providers, and improve main bootstrap process

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { Project } from './projects/project.entity';
 import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         type: 'mysql',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
@@ -19,13 +23,11 @@ import { ProjectsModule } from './projects/projects.module';
         database: configService.get<string>('DB_DATABASE'),
         entities: [Project],
         synchronize: true,
-        ssl:
-          configService.get<string>('NODE_ENV') === 'production'
-            ? { rejectUnauthorized: false }
-            : null,
       }),
     }),
     ProjectsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as serverless from 'serverless-http';
 import { AppModule } from './app.module';
 
-const server = express();
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
-  return server;
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 5000);
 }
-
-// Export the serverless handler
-export const handler = serverless(await bootstrap());
+bootstrap();
