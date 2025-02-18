cd39cade287680216c000ab1931ede2823941cf0
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-02-19 00:10:59 +0600

    feat: integrate serverless support with Express adapter in main bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as serverless from 'serverless-http';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 5000);
+  await app.init();
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+  return serverless(expressApp);
 }
-bootstrap();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+export const handler = bootstrap().then((app) => app);
