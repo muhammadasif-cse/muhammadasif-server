7b8192aed34c31af143ba0e4fc685022f51be07c
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-02-19 00:13:17 +0600

    feat: remove Vercel dependency and clean up main bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   await app.init();
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
   return serverless(expressApp);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 export const handler = bootstrap().then((app) => app);
