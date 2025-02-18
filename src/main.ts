4a4dc55fbb82f2f2f857830b838e16bc2da0c5e7
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-02-19 00:30:36 +0600

    feat: update Vercel configuration to use TypeScript entry point and remove serverless dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { Handler } from 'aws-lambda'; // Import the Handler type for serverless functions
-import * as express from 'express';
-import * as serverless from 'serverless-http';
 import { AppModule } from './app.module';
 
-async function bootstrap(): Promise<Handler> {
-  const expressApp = express();
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.init();
-
-  // Wrap the Express app with serverless-http and return the handler
-  return serverless(expressApp);
+  await app.listen(process.env.PORT ?? 5000);
 }
-
-// Export the handler for Vercel
-export const handler = bootstrap();
+bootstrap();
