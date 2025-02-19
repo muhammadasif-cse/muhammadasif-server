f5d7848a48c690d9d2e98263d47020c74223cc78
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-02-19 22:40:53 +0600

    feat: update greeting messages and method names in AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { ProjectsModule } from './projects/projects.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
         entities: [Project],
-        synchronize: true,
+        synchronize: false,
       }),
     }),
     ProjectsModule,
@@ -31,3 +31,20 @@ import { ProjectsModule } from './projects/projects.module';
   providers: [AppService],
 })
 export class AppModule {}
+// {
+//   const appOptions = { cors: true };
+//   const app = await NestFactory.create(ApplicationModule, appOptions);
+//   app.setGlobalPrefix('api');
+
+//   const options = new DocumentBuilder()
+//     .setTitle('NestJS Realworld Example App')
+//     .setDescription('The Realworld API description')
+//     .setVersion('1.0')
+//     .setBasePath('api')
+//     .addBearerAuth()
+//     .build();
+//   const document = SwaggerModule.createDocument(app, options);
+//   SwaggerModule.setup('/docs', app, document);
+
+//   await app.listen(3000);
+// }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getService(): string {
     return 'Server is running';
   }
 }
