9229c9515809631e395821d484db3c1f8134a811
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-02-19 22:32:09 +0600

    feat: remove ESLint configuration and update TypeScript settings in project files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 5000);
+
+  await app.listen(5019);
 }
 bootstrap();
